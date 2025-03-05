1b7ed3dfd1a6c8296d7014912cb0164df444d1b2
Nibedan7 <nibedanpattanaik6@gmail.com>
2025-03-05 15:55:20 +0530

    Crud Operation

---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [DisplayName("Category")]
+        [MaxLength(30, ErrorMessage = "Name must in between 30 character")]
+        public string Name { get; set; }
+        [DisplayName("Display order")]
+        [Range(1,100, ErrorMessage = "Display Order must be Ranges between(1-100)") ]
+        public int DisplayOrder { get; set; }
+    }
+}
